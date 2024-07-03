ddefa917aef619eefa293c8ab5b41553ccbd616c
Henry Pick <1henrypick@gmail.com>
2024-07-02 22:31:29 -0700

    gui and double slider

--- src/gui.rs
+++ src/gui.rs
@@ -1,37 +1,99 @@
-use iced::Sandbox;
+use std::fmt::Display;
+mod double_slider;
 
-struct ControlBar {
-    frequency_slider: FrequencySlider,
-    mode_selection: ModeSelection,
-}
-
-struct FrequencySlider {
-    left_bound: u32,
-    right_bound: u32,
-}
-
-struct ModeSelection {
-    mode: DisplayMode,
-}
+use double_slider::DoubleSlider;
+use iced::widget::horizontal_space;
+use iced::widget::pick_list;
+use iced::widget::row;
+use iced::{Alignment, Sandbox};
 
 // TODO: add more modes and move this to a new module!
-enum DisplayMode {
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum DisplayMode {
     Rolling,
     Power,
     Frequency,
 }
 
-#[derive(Debug, Clone, Copy)]
-enum ModeSelected {
-    Selected,
+impl DisplayMode {
+    const ALL: [DisplayMode; 3] = [
+        DisplayMode::Rolling,
+        DisplayMode::Power,
+        DisplayMode::Frequency,
+    ];
 }
-//impl Sandbox for ModeSelection {
-//    type Message = ModeSelected;
-//
-//    fn new() -> Self {
-//        Self {
-//            mode: DisplayMode::Frequency,
-//        }
-//    }
-//}
-//
+
+impl Display for DisplayMode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                DisplayMode::Rolling => "Rolling",
+                DisplayMode::Power => "Power",
+                DisplayMode::Frequency => "Frequency",
+            }
+        )
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum GuiMessage {
+    ModeSelected(DisplayMode),
+    LeftSlider(i32),
+    RightSlider(i32),
+}
+
+pub struct Gui {
+    selected_mode: Option<DisplayMode>,
+    left_slider: i32,
+    right_slider: i32,
+}
+
+impl Sandbox for Gui {
+    type Message = GuiMessage;
+
+    fn new() -> Self {
+        Self {
+            selected_mode: Some(DisplayMode::Frequency),
+            left_slider: 0,
+            right_slider: 0,
+        }
+    }
+
+    fn title(&self) -> String {
+        "Audio Reactive LED Strip".to_string()
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        match message {
+            GuiMessage::ModeSelected(mode) => {
+                self.selected_mode = Some(mode);
+            }
+            GuiMessage::LeftSlider(value) => self.left_slider = value,
+            GuiMessage::RightSlider(value) => self.right_slider = value,
+        }
+    }
+
+    fn view(&self) -> iced::Element<'_, Self::Message> {
+        let mode_select = pick_list(
+            &DisplayMode::ALL[..],
+            self.selected_mode,
+            GuiMessage::ModeSelected,
+        );
+
+        let slider = DoubleSlider::new(0..=20000, self.left_slider, GuiMessage::LeftSlider);
+
+        let controls_bar = row![
+            horizontal_space().width(30),
+            mode_select,
+            slider,
+            horizontal_space().width(30)
+        ]
+        .height(100)
+        .align_items(Alignment::End)
+        .spacing(10);
+
+        controls_bar.into()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,55 +5,10 @@ mod gamma_table;
 mod gui;
 mod led;
 
-use iced::widget::{button, column, text};
-use iced::{Alignment, Element, Sandbox, Settings};
+use iced::{Sandbox, Settings};
 
 pub fn main() -> iced::Result {
-    Counter::run(Settings::default())
-}
-
-struct Counter {
-    value: i32,
-}
-
-#[derive(Debug, Clone, Copy)]
-enum Message {
-    IncrementPressed,
-    DecrementPressed,
-}
-
-impl Sandbox for Counter {
-    type Message = Message;
-
-    fn new() -> Self {
-        Self { value: 0 }
-    }
-
-    fn title(&self) -> String {
-        String::from("Counter - Iced")
-    }
-
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::IncrementPressed => {
-                self.value += 1;
-            }
-            Message::DecrementPressed => {
-                self.value -= 1;
-            }
-        }
-    }
-
-    fn view(&self) -> Element<Message> {
-        column![
-            button("Increment").on_press(Message::IncrementPressed),
-            text(self.value).size(50),
-            button("Decrement").on_press(Message::DecrementPressed)
-        ]
-        .padding(20)
-        .align_items(Alignment::Center)
-        .into()
-    }
+    gui::Gui::run(Settings::default())
 }
 
 // for starters, let's build a gui that has a slider and a gl context or cairo rendering area
