b22b1fe5bbc703feca6568d4d67a76b9bcbe3c6c
Henry Pick <1henrypick@gmail.com>
2024-12-27 13:17:42 -0800

    default dark theme

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ pub fn main() -> iced::Result {
     {
         iced::application("Audio Reactive Renderer", Gui::update, Gui::view)
             .subscription(Gui::subscription)
+            .theme(|_| iced::Theme::Dark)
             .exit_on_close_request(false)
             .run()
     }
